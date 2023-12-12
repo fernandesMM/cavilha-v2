96208892ae1fda300548a38a6475a39ff7431470
fernandesMM <roberto.fernandes@madeiramadeira.com.br>
2023-12-12 11:47:53 -0300

    Removing dist

--- dist/cjs/components/TextField/TextField.d.ts
+++
@@ -1,6 +0,0 @@
-import React, { InputHTMLAttributes } from "react";
-export interface Props extends InputHTMLAttributes<HTMLInputElement> {
-    label: string;
-}
-declare function TextField(props: Props): React.JSX.Element;
-export default TextField;
